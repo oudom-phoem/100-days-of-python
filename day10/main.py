db2c82c155c011df259a823617269f8954aa7366
Oudom Phoem <oudomphoem@gmail.com>
2024-12-17 17:01:13 +0700

    Add day10

---
+++ day10/main.py
@@ -0,0 +1,58 @@
+import os
+import art
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def clear_console():
+  """Clears the console."""
+  command = 'cls' if os.name == 'nt' else 'clear'
+  os.system(command)
+
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            clear_console()
+            calculator()
+
+
+calculator()
